320df41029da797c6b5149b6ece725c0aea4b0d0
NoorPoppe <noor.poppe@student.howest.be>
2021-04-18 16:44:34 +0200

    nav stytles

--- components/nav.js
+++ components/nav.js
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import Image from 'next/image'
-import styles from './nav.module.css'
+import styles from '../styles/nav.module.css'
 
 const Nav = () => {
     return (
