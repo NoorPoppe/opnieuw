d977e2d7c28fab2b9b7e83f2d12f7c932c396120
NoorPoppe <noor.poppe@student.howest.be>
2021-04-25 12:42:15 +0200

    createmessage test

--- pages/createMessage/[createMessage].js
+++ pages/createMessage/[createMessage].js
@@ -1,4 +1,15 @@
+import { useRouter } from 'next/router'
+
+//import { fetchEntries } from '@utils/contentfulPosts'
 
 export default function CreateMessages() {
+    const router = useRouter()
+    return (
+        <>
+            <div className="container">
+                <p>test</p>
+            </div>
+        </>
+    )
 }
 
