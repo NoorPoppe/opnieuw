ddc6a9e63639b1c1adba32ca8f2e6f480c1a6265
NoorPoppe <noor.poppe@student.howest.be>
2021-04-25 12:39:15 +0200

    creatte message leegmaken

--- pages/createMessage/[createMessage].js
+++ pages/createMessage/[createMessage].js
@@ -1,42 +1,4 @@
-import { useRouter } from 'next/router'
-import { fetchEntries } from '../../utils/contentfulCats'
-import CreateMessage from '../../components/CreateMessage'
 
-export default function CreateMessages({ content }) {
-    console.log(content)
-    const router = useRouter()
-    return (
-        <>
-            <div className="container">
-                <title>{content.to}</title>
-                <CreateMessage />
-            </div>
-        </>
-    )
+export default function CreateMessages() {
 }
 
-export async function getStaticPaths() {
-    const res = await fetchEntries()
-    const cornucopia = await res.map((p) => {
-        return `/createMessage/`
-    })
-
-    return { paths: cornucopia, fallback: false }
-}
-
-export async function getStaticProps({ params }) {
-    const res = await fetchEntries()
-    const content = await res
-        .map((p) => {
-            return p.fields
-        })
-        .filter((thing) => {
-            return thing.slug === params.createMessage
-        })
-
-    return {
-        props: {
-            content: content[0],
-        },
-    }
-}
